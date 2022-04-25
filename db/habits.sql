b5ec544094915f3a1839593ec77bd50bce23a3f8
Prishal <92685854+Prishal-git@users.noreply.github.com>
2022-04-25 16:34:11 +0100

    added the two SQL tables

---
+++ db/habits.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS habits;
+
+CREATE TABLE habits (
+    id serial PRIMARY KEY,
+    user_id int NOT NULL
+    name ENUM ('Water', 'Exercise', 'Sleep') NOT NULL,
+    frequency int NOT NULL,
+    frequency_count int,
+);
