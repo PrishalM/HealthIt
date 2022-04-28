ccb6537dff7f6bce5f9ce0a7e17184f12bb9c24f
Prishal <prishalmakwana@gmail.com>
2022-04-28 16:30:03 +0100

    added changes

--- client/scripts/index.js
+++ client/scripts/index.js
@@ -1,52 +1,45 @@
-const express = require('express');
-const router = express.Router();
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
 ////////////////////////////////////////////////////////////////
 // Index event listeners
 
-
 // Sign in button
-document.getElementById("SIbutton").addEventListener("click", async e => {
+document.getElementById("SIbutton").addEventListener("click", login);
+
+async function login(e) {
   e.preventDefault();
-  
-  async function login(e){
-    // e.preventDefault();
-    let userInputEmail = document.getElementById("SIemail").value;
-    let userInputPassword = document.getElementById("SIpassword").value;
-    if (userInputEmail.length > 0 && userInputPassword.length > 0) {
-      try {
-        const login = {
-          email: userInputEmail,
-          password: userInputPassword
-        };
 
-        const options = {
-          method: 'POST',
-          headers: { "Content-type": "application/json" },
-          body: JSON.stringify(login)
-        };
-          console.log(login);
-        const response = await fetch ("http://localhost:3000/auth", options);
-        if (response.status == 200) {
-          //Store the token
-          window.location.assign("dashboard.html");
-        }
-      } 
-        catch (err) {
-        console.warn(err);
+  let userInputEmail = document.getElementById("SIemail").value;
+  let userInputPassword = document.getElementById("SIpassword").value;
+  if (userInputEmail.length > 0 && userInputPassword.length > 0) {
+    try {
+      const login = {
+        email: userInputEmail,
+        password: userInputPassword,
       };
-    } else {
-      alert(
-        "Incorrect email and password combination. Please try again."
-      );
-    }
-  };
-  login();
-}
-);
-  
 
+      const options = {
+        method: "POST",
+        headers: { "Content-type": "application/json" },
+        body: JSON.stringify(login),
+      };
+      console.log(login);
+      const response = await fetch("http://localhost:3000/auth/login", options);
+      console.log(response.status);
+      // if (response.status == 200) {
+      //   console.log(re);
+      // }
+      const data = await response.json();
+
+      if (!data.success) {
+        alert("Incorrect email and password combination. Please try again.");
+        throw new Error("Login not authorised!");
+      }
+      localStorage.setItem("token", data.token);
+      window.location.assign("dashboard.html");
+    } catch (err) {
+      console.warn(err);
+    }
+  }
+}
 // Create account button
 document.getElementById("CAbutton").addEventListener("click", createAccount);
 
