640530a05c2b71d6f25ba9f179d0194fa0f562a8
Diren <nayir_da@hotmail.co.uk>
2022-04-28 18:56:12 +0100

    create account works - password is hashed and stored before redirecting to sign in page

--- client/scripts/index.js
+++ client/scripts/index.js
@@ -46,37 +46,65 @@ async function login(e) {
 // Create account button
 document.getElementById("CAbutton").addEventListener("click", createAccount);
 
-async function createAccount() {
+// async function createAccount() {
+//   let name = document.getElementById("CAname").value;
+//   let email = document.getElementById("CAemail").value;
+//   let password = document.getElementById("CApassword").value;
+//   if (name.length > 0 && email.length > 0 && password.length > 0) {
+//     try {
+//       let newUser = {
+//         username: name,
+//         email: email,
+//         password: password,
+//       };
+
+//       const options = {
+//         method: "POST",
+//         headers: { "Content-Type": "application/json" },
+//         body: JSON.stringify(newUser),
+//       };
+
+//       const response = await fetch("http://localhost:3000/users", options);
+//       const { id, err } = await response.json();
+//       if (err) {
+//         throw Error(err);
+//       } else {
+//       }
+//     } catch (err) {
+//       console.warn(err);
+//     }
+//   } else {
+//     alert(
+//       "Name, Email and Password fields are mandatory to create user. Please try again."
+//     );
+//   }
+// }
+async function createAccount(e) {
+  e.preventDefault();
   let name = document.getElementById("CAname").value;
   let email = document.getElementById("CAemail").value;
   let password = document.getElementById("CApassword").value;
-  if (name.length > 0 && email.length > 0 && password.length > 0) {
-    try {
-      let newUser = {
-        username: name,
-        email: email,
-        password: password,
-      };
-
-      const options = {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(newUser),
-      };
-
-      const response = await fetch("http://localhost:3000/users", options);
-      const { id, err } = await response.json();
-      if (err) {
-        throw Error(err);
-      } else {
-      }
-    } catch (err) {
-      console.warn(err);
+  try {
+    let newUser = {
+      username: name,
+      email: email,
+      password: password,
+    };
+    const options = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(newUser),
+    };
+    const r = await fetch(`http://localhost:3000/auth/register`, options);
+    const data = await r.json();
+    console.log(data);
+    if (data.err) {
+      throw Error(data.err);
     }
-  } else {
-    alert(
-      "Name, Email and Password fields are mandatory to create user. Please try again."
-    );
+    // window.location.hash = '#login';
+    window.location.assign("index.html");
+  } catch (err) {
+    console.warn(err);
   }
 }
 
