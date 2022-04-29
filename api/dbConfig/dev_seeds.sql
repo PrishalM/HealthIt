81cc074f4f7a1292bb586f281b1bc2cad3841337
Prishal <prishalmakwana@gmail.com>
2022-04-29 15:19:27 +0100

    updated hardcoded habits

--- api/dbConfig/dev_seeds.sql
+++ api/dbConfig/dev_seeds.sql
@@ -1,13 +1,8 @@
 INSERT INTO habits (habit_name, frequency, user_id)
-VALUES ('Sleep', 3, 2);
+VALUES ('Exercise', 3, 1), ('Water', 3, 1), ('Sleep', 3, 1), ('Sleep', 3, 2), ('Exercise', 3, 2), ('Sleep', 3, 3);
 
-INSERT INTO habits (user_id, habit_name, frequency)
-VALUES
-(1, 'Sleep', 3),
-(2, 'Water', 2),
-(3, 'Exercise', 2);
 INSERT INTO users (username, email, password, habit_id)
 VALUES
-('JohnDoe', 'johndoe@gmail.com', '$2y$10$nrcLfGLJ2ZcwhsAu/qrUQOyoPmycazmiiJOkVATEprECVW/b1OlwC', 1),
-('JaneDoe', 'janedoe@gmail.com', '$2y$10$Z17QToHQIJV8uCK3VH0pIu/MZgsZPmIUVyvXXAjMmng/w24qXcpH.', 2),
-('DummyName', 'dummy@gmail.com', '$2y$10$4fMJ4nxJ8L39sho2wAcfWONlgZO9icT4m7lZ7JnCX9mDVBCzrm0wK', 3);
+('Diren', 'Diren@gmail.com', '$2y$10$nrcLfGLJ2ZcwhsAu/qrUQOyoPmycazmiiJOkVATEprECVW/b1OlwC', 1),
+('Doreen', 'Doreen@gmail.com', '$2y$10$Z17QToHQIJV8uCK3VH0pIu/MZgsZPmIUVyvXXAjMmng/w24qXcpH.', 2),
+('Prishal', 'Prishal@gmail.com', '$2y$10$4fMJ4nxJ8L39sho2wAcfWONlgZO9icT4m7lZ7JnCX9mDVBCzrm0wK', 3);
